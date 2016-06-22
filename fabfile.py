eee8df60cf074c73285172f6705cd61719a9175b
chrisclark <chris@untrod.com>
2016-06-21 17:04:31 -0700

    purge cloudflare cache after publish

--- fabfile.py
+++ fabfile.py
@@ -7,6 +7,8 @@ import SocketServer
 from livereload import Server
 from pelican import Pelican
 from pelican.settings import read_settings
+import requests
+import json
 
 # Local path configuration (can be absolute or relative to fabfile)
 env.deploy_path = 'output'
@@ -53,8 +55,26 @@ def dev():
     server.serve(root='output')
 
 
+def clear_cache():
+    # Purges the CloudFlare cache.
+    # Zone was retrieved using:
+    # curl -X GET "https://api.cloudflare.com/client/v4/zones?name=untrod.com&status=active&page=1&per_page=20&order=status&direction=desc&match=all" \
+    # -H "X-Auth-Email: <email>" \
+    # -H "X-Auth-Key: <key>" \
+    # -H "Content-Type: application/json"
+
+    cf_zone = 'a5f5672c9471d5fca08912db32d7f1d3'
+    cf_auth_key = '55edefc1c418ca263eea896623e6d66c0e4d2'
+    cf_email = 'chris@untrod.com'
+    url = "https://api.cloudflare.com/client/v4/zones/%s/purge_cache" % cf_zone
+    headers = {'X-Auth-Email': cf_email, 'X-Auth-Key': cf_auth_key, 'Content-Type': 'application/json'}
+    data = {'purge_everything': True}
+    requests.delete(url, data=json.dumps(data), headers=headers)
+
+
 def publish():
     local('make s3_upload')
+    clear_cache()
 
 
 def preview():
